72406f5315d7819f84aff8c5d2edc691eca4f375
Getulio Vieira de Moraes <getulio.vieira@fariasbrito.com.br>
2021-04-18 15:58:45 -0300

    Episode Detail Screen

--- src/screens/EpisodeDetail/index.tsx
+++ src/screens/EpisodeDetail/index.tsx
@@ -1,16 +1,43 @@
+import moment from "moment";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View, ScrollView } from "react-native";
+import { primary_gray } from "../../constants";
+import { styles } from "./styles";
 
 interface Props {
   route: any;
 }
 const EpisodeDetail: React.FC<Props> = ({ route }) => {
   const { episode } = route.params;
-  console.log(episode);
+
   return (
-    <View>
-      <Text>Espisode Detail</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.header}>
+        <Text style={styles.title}>{episode.name}</Text>
+        <Image style={styles.image} source={{ uri: episode.image.medium }} />
+        <Text style={styles.summary}>
+          {episode.summary
+            .replace(/<[^>]*>/g, " ")
+            .replace(/\s{2,}/g, " ")
+            .trim()}
+        </Text>
+      </View>
+      <View style={{ padding: 20 }}>
+        <Text style={styles.title}>Episode Info</Text>
+        <View style={styles.detailRow}>
+          <Text style={styles.fontOption}>Air Date: </Text>
+          <Text style={{ color: primary_gray }}>
+            {moment.utc(episode.airstamp).format("LL")}
+          </Text>
+        </View>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Text style={styles.fontOption}>Number: </Text>
+          <Text style={{ color: primary_gray }}>
+            Season {episode.season}, Episode {episode.number}{" "}
+          </Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
--- src/wrappers/index.ts
+++ src/wrappers/index.ts
@@ -1,1 +1,2 @@
 export { default as LoadingShows } from "./LoadingShows";
+export { default as LoadingEpisodes } from "./LoadingEpisodes";
