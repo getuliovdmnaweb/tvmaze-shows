f0c2a023f1136fcff047c6834b975d3bd5450773
Getulio Vieira de Moraes <getulio.vieira@fariasbrito.com.br>
2021-04-19 02:32:49 -0300

    Custom hook for retrieving people shows

--- src/hooks/useSeasonEpisodes.ts
+++ src/hooks/useSeasonEpisodes.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useAppDispatch } from "./useRedux";
-import { setLoadingEpisodes } from "../redux/actions/shows";
+import { setLoadingEpisodes } from "../redux/actions";
 import { Episode, Show } from "../types";
 
 export const useSeasonEpisodes = (show: Show) => {
