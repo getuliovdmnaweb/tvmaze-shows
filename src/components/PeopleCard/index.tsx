c3eba4e3847b91a8cbb9b1414e36354b36cd3f02
Getulio Vieira de Moraes <getulio.vieira@fariasbrito.com.br>
2021-04-19 02:33:40 -0300

    Adding People Detail screen

--- src/components/PeopleCard/index.tsx
+++ src/components/PeopleCard/index.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
-import { MaterialIcons } from "@expo/vector-icons";
 import { People } from "../../types";
 import { styles } from "./styles";
+import { useNavigation } from "@react-navigation/core";
 
 interface Props {
   people: People;
 }
 
 const ShowCard: React.FC<Props> = ({ people }) => {
+  const navigation = useNavigation();
   return (
     <View style={styles.card}>
-      <TouchableOpacity accessibilityRole="button">
+      <TouchableOpacity
+        onPress={() => navigation.navigate("People Detail", { person: people })}
+        accessibilityRole="button"
+      >
         <Image style={styles.image} source={{ uri: people.image?.medium }} />
       </TouchableOpacity>
 
