b10048508ac8151e640d79d41e01a1bf98c13266
Getulio Vieira de Moraes <getulio.vieira@fariasbrito.com.br>
2021-04-18 22:40:38 -0300

    List People Screen and Wrapper

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -2,3 +2,4 @@ export { default as ListShows } from "./ListShows";
 export { default as ShowDetail } from "./ShowDetail";
 export { default as FavoriteShows } from "./FavoriteShows";
 export { default as EpisodeDetail } from "./EpisodeDetail";
+export { default as ListPeople } from "./ListPeople";
--- src/wrappers/index.ts
+++ src/wrappers/index.ts
@@ -1,2 +1,4 @@
 export { default as LoadingShows } from "./LoadingShows";
 export { default as LoadingEpisodes } from "./LoadingEpisodes";
+export { default as SortingShows } from "./SortingShows";
+export { default as LoadingPeople } from "./LoadingPeople";
